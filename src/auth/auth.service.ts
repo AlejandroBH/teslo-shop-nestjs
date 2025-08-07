c8aee860893e96a0717f194ea2dec4cc5948954c
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-08-07 18:31:02 -0400

    login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,13 @@ import {
   BadRequestException,
   InternalServerErrorException,
   Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -19,13 +21,40 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
       await this.userRepository.save(user);
+      delete user.password;
 
       return user;
+      // TODO: Retornar el JWT de acceso
     } catch (error) {
       this.handleDBExeptions(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException(`Credentials are not valid (email)`);
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException(`Credentials are not valid (password)`);
+    }
+
+    return user;
+    // TODO: Retornar el JWT de acceso
   }
 
   private handleDBExeptions(error: any): never {
@@ -33,7 +62,7 @@ export class AuthService {
       throw new BadRequestException(error.detail);
     }
 
-    console.log(error);
+    this.logger.error(error);
     throw new InternalServerErrorException(
       'Unexpected error, check server logs',
     );
