7145ab7ef08cfb65a1310867efe367dedd9843c5
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-08-09 19:26:50 -0400

    check auth status

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -32,6 +32,12 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
+  @Get('check-status')
+  @Auth()
+  checkAuthStatus(@GetUser() user: User) {
+    return this.authService.checkAuthStatus(user);
+  }
+
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
@@ -42,7 +48,7 @@ export class AuthController {
     @RawHeaders() rawHeaders: string[],
     @Headers() headers: IncomingHttpHeaders,
   ) {
-    console.log(request);
+    // console.log(request);
 
     return {
       ok: true,
